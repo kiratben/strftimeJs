a369369fc86693b4eb7992d826e21f727acc55c8
tben1 <tarikbenabbou@gmail.com>
2016-08-27 01:15:44 +0100

    init

---
+++ strftime.js
@@ -0,0 +1,25 @@
+(function () {
+	'use strict';
+
+	var defaultLocale = {
+			days: 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(' '),
+			shortDays: 'Sun Mon Tue Wed Thu Fri Sat'.split(' '),
+			months: 'January February March April May June July August September October November December'.split(' '),
+			shortMonths: 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+			AM: 'AM',
+			PM: 'PM',
+			am: 'am',
+			pm: 'pm',
+
+			formats: {
+				D: '%m/%d/%y',
+				F: '%Y-%m-%d',
+				e: '%e-%b-%Y',
+				R: '%H:%M',
+				r: '%I:%M:%S %p',
+				T: '%H:%M:%S'
+			}
+		};
+
+
+})();
